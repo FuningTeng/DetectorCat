877a1be8084c29cde8b071e0e6ab314754d6214a
Funing Teng <funingteng@gmail.com>
2017-11-11 21:02:06 -0600

    conneted to filebase server

--- client-app/src/environments/environment.ts
+++ client-app/src/environments/environment.ts
@@ -4,5 +4,13 @@
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyCgCpKWdggcV-l9pbqn27PEXv5JdgAk8AE',
+    authDomain: 'detectorcat.firebaseapp.com',
+    databaseURL: 'https://detectorcat.firebaseio.com',
+    projectId: 'detectorcat',
+    storageBucket: 'detectorcat.appspot.com',
+    messagingSenderId: '678241272183'
+  }
 };
